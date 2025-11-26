c6095eac5d207cee9f379a6dab0654c6b41fae90
rahman318 <rahman_uwl@edenzil.com>
2025-11-26 12:43:25 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login({ setUser }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://backenduwleapprovalsystem.onrender.com/api/auth/login", {
         email,
         password,
       });
