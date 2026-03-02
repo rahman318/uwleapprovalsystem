b0c90ffcd6785f9aed4192233fdbf2a6cd6a9f85
rahman318 <rahman_uwl@edenzil.com>
2026-03-02 11:35:38 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -162,13 +162,6 @@ export default function Login({ setUser }) {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <button
-            type="button"
-            onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-3 top-9 text-gray-500 hover:text-gray-700"
-          >
-            {showPassword ? "🙈" : "👁️"}
-          </button>
           </div>
 
           <button
