db98785ae86e36a9fcf289955ec7e448556ffc6f
rahman318 <rahman_uwl@edenzil.com>
2026-04-29 19:42:01 +0800

    Add files via upload

---
+++ src/components/TickerAdmin.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const TickerBar = () => {
+  const [ticker, setTicker] = useState([]);
+
+  const fetchTicker = async () => {
+    const res = await fetch("http://localhost:3000/api/ticker");
+    const data = await res.json();
+    setTicker(data);
+  };
+
+  useEffect(() => {
+    fetchTicker();
+  }, []);
+
+  return (
+    <div className="w-full overflow-hidden bg-slate-900 text-white py-2">
+      <div className="ticker">
+        {ticker.map((t) => (
+          <span key={t._id}>
+            {t.message} &nbsp;&nbsp;&nbsp;
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TickerBar;
